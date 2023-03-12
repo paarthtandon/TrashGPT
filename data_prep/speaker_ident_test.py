7289b1a3eecaa3db9a108b4fc1d96088ae0f461b
Paarth Tandon <paarthri@gmail.com>
2023-03-12 14:47:05 -0400

    remove all non-ascii chars as well

--- data_prep/speaker_ident_test.py
+++ data_prep/speaker_ident_test.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from pyannote.audio import Pipeline
+import time
 
 load_dotenv()
 HF_TOKEN = os.getenv('HF')
@@ -10,10 +11,12 @@ pipeline = Pipeline.from_pretrained('pyannote/speaker-diarization@2.1', use_auth
 
 print('pipeline loaded')
 
+start_time = time.time()
+
 # apply the pipeline to an audio file
 diarization = pipeline(
-    data_dir + 'Animals We Could Beat in a Fight ｜ Trash Taste #91 [ZWS2nFo7eEo].wav' # ,
-    # min_speakers = 3
+    data_dir + 'Animals_We_Could_Beat_in_a_Fight__Trash_Taste_91_ZWS2nFo7eEo.wav',
+    min_speakers = 3
 )
 
 print('diarization complete')
@@ -21,3 +24,5 @@ print('diarization complete')
 # dump the diarization output to disk using RTTM format
 with open("audio.rttm", "w") as rttm:
     diarization.write_rttm(rttm)
+
+print("--- %s seconds ---" % (time.time() - start_time))
