d4cb4f21ec8ec02914849974038de61aeb747514
Miyamoto-Musashi <Mohamedkhaled228@gmail.com>
2023-03-13 01:35:22 -0400

    jeej
    too tired for this shit, here are the files with speakers split.

--- data_prep/speaker_ident_test.py
+++ data_prep/speaker_ident_test.py
@@ -2,10 +2,13 @@ import os
 from dotenv import load_dotenv
 from pyannote.audio import Pipeline
 import time
+import sys
 
 load_dotenv()
 HF_TOKEN = os.getenv('HF')
+print(HF_TOKEN)
 data_dir = '../data/'
+fileName = sys.argv[1]
 
 pipeline = Pipeline.from_pretrained('pyannote/speaker-diarization@2.1', use_auth_token=HF_TOKEN)
 
@@ -15,14 +18,14 @@ start_time = time.time()
 
 # apply the pipeline to an audio file
 diarization = pipeline(
-    data_dir + 'Animals_We_Could_Beat_in_a_Fight__Trash_Taste_91_ZWS2nFo7eEo.wav',
+    data_dir + fileName,
     min_speakers = 3
 )
 
 print('diarization complete')
 
 # dump the diarization output to disk using RTTM format
-with open(f'audio.rttm', "w") as rttm:
+with open(f'{fileName}.rttm', "w") as rttm:
     diarization.write_rttm(rttm)
 
 print("--- %s seconds ---" % (time.time() - start_time))
