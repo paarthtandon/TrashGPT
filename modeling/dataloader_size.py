1eefce326c7e7d1a6f7a3f7967a0872b2820f2f8
Paarth Tandon <paarthri@gmail.com>
2023-04-24 14:43:13 -0400

    gpt2 code

--- modeling/dataloader_size.py
+++ modeling/dataloader_size.py
@@ -1,9 +1,12 @@
 from torch.utils.data import DataLoader
 from custom_datasets import GPT2Transcript
+from transformers import GPT2Tokenizer
+
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium', pad_token='<|pad|>')
 
 dataset_fn = '../annotated_w_names/_dataset.txt'
 dataset_f = open(dataset_fn, 'r', encoding='utf-8')
-dataset = GPT2Transcript(dataset_f.read(), gpt2_type="gpt2")
+dataset = GPT2Transcript(dataset_f.read(), tokenizer)
 dataset_f.close()
 
 dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
--- modeling/train_gpt2_new.py
+++ modeling/train_gpt2_new.py
@@ -1,14 +1,12 @@
-import pandas as pd
 import torch
-from torch.utils.data import Dataset, random_split
 from transformers import GPT2Tokenizer, TrainingArguments, Trainer, GPT2LMHeadModel
 from custom_datasets import GPT2Transcript
 
 dataset_fn = '../annotated_w_names/_dataset.txt'
-model_dir = 'models/gpt_med'
+model_dir = 'models/gpt_small'
 
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium', pad_token='<|pad|>')
-model = GPT2LMHeadModel.from_pretrained('gpt2-medium').cuda()
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2', pad_token='<|pad|>')
+model = GPT2LMHeadModel.from_pretrained('gpt2').cuda()
 model.resize_token_embeddings(len(tokenizer))
 
 dataset_f = open(dataset_fn, 'r', encoding='utf-8')
@@ -17,7 +15,18 @@ dataset_f.close()
 
 torch.cuda.empty_cache()
 
-training_args = TrainingArguments(output_dir=model_dir,
+training_args_small = TrainingArguments(output_dir=model_dir,
+                                  num_train_epochs=2,
+                                  logging_steps=100,
+                                  save_steps=5000,
+                                  per_device_train_batch_size=2,
+                                  per_device_eval_batch_size=2,
+                                  warmup_steps=10,
+                                  weight_decay=0.05,
+                                  logging_dir=model_dir,
+                                  report_to='none')
+
+training_args_med = TrainingArguments(output_dir=model_dir,
                                   num_train_epochs=2,
                                   logging_steps=100,
                                   save_steps=5000,
@@ -29,7 +38,7 @@ training_args = TrainingArguments(output_dir=model_dir,
                                   report_to='none')
 
 trainer = Trainer(model=model,
-        args=training_args,
+        args=training_args_small,
         train_dataset=dataset,
         data_collator=lambda data: {
                 'input_ids': torch.stack([f[0] for f in data]),
@@ -39,4 +48,4 @@ trainer = Trainer(model=model,
         )
 
 trainer.train()
-trainer.save_model(model_dir + 'gpt2med_final.pt')
+trainer.save_model(model_dir + 'gpt2small_final.pt')
