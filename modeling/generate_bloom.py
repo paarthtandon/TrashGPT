2a4a51cad26f898b34ed186f72e482d40efc66fc
Paarth Tandon <paarthri@gmail.com>
2023-04-25 00:48:29 +0000

    bloom training

--- modeling/generate_bloom.py
+++ modeling/generate_bloom.py
@@ -6,19 +6,18 @@ from tqdm import trange
 device = torch.device("cuda")
 
 tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-560m")
-model = BloomForCausalLM.from_pretrained("bigscience/bloom-560m").cuda()
-# model.load_state_dict(torch.load('models/gpt_med/checkpoint-35000/pytorch_model.bin'))
+model = BloomForCausalLM.from_pretrained("bigscience/bloom-560m", device_map="auto", torch_dtype=torch.float16).cuda()
+model.load_state_dict(torch.load('models/bloom_560_final/pytorch_model.bin'))
 
 # encode context the generation is conditioned on
-input_ids = tokenizer.encode('<title> global warming </title>', return_tensors='pt').to(device)
+input_ids = tokenizer.encode('<title>', return_tensors='pt').to(device)
 
 # activate sampling and deactivate top_k by setting top_k sampling to 0
 sample_output = model.generate(
     input_ids, 
     do_sample=True, 
-    max_length=2048, 
-    # top_k=50, 
-    top_p=0.75
+    max_length=2048,
+    top_p=0.85
 )
 
 print(tokenizer.decode(sample_output[0], skip_special_tokens=True))
--- modeling/train_bloom.py
+++ modeling/train_bloom.py
@@ -6,7 +6,7 @@ dataset_fn = '../annotated_w_names/_dataset.txt'
 model_dir = 'models/bloom_560'
 
 tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-560m")
-model = BloomForCausalLM.from_pretrained("bigscience/bloom-560m", device_map="auto", load_in_8bit=True).cuda()
+model = BloomForCausalLM.from_pretrained("bigscience/bloom-560m").cuda()
 
 dataset_f = open(dataset_fn, 'r', encoding='utf-8')
 dataset = BloomTranscript(dataset_f.read(), tokenizer)
@@ -17,13 +17,17 @@ torch.cuda.empty_cache()
 training_args = TrainingArguments(output_dir=model_dir,
                                   num_train_epochs=2,
                                   logging_steps=10,
+                                  # save_steps=100,
                                   save_steps=5000,
                                   per_device_train_batch_size=1,
                                   per_device_eval_batch_size=1,
                                   warmup_steps=10,
                                   weight_decay=0.05,
                                   logging_dir=model_dir,
-                                  report_to='none')
+                                  report_to='none',
+                                  fp16=True,
+                                  gradient_accumulation_steps=4,
+                                  gradient_checkpointing=True)
 
 trainer = Trainer(model=model,
         args=training_args,
