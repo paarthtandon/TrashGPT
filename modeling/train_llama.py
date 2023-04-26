cba769f615df5e6f2dbdd30f1a3b5e53878c049c
Paarth Tandon <paarthri@gmail.com>
2023-04-26 14:47:03 -0400

    llama training code

--- modeling/train_llama.py
+++ modeling/train_llama.py
@@ -1,44 +1,74 @@
 import torch
-from transformers import LlamaTokenizer, LlamaForCausalLM, TrainingArguments, Trainer
+from transformers import LlamaTokenizer, LlamaForCausalLM, TrainingArguments, Trainer, DataCollatorForLanguageModeling
+from peft import LoraConfig, get_peft_model 
 from custom_datasets import BloomTranscript
 
 dataset_fn = '../annotated_w_names/_dataset.txt'
 model_dir = 'models/llama'
 
 tokenizer = LlamaTokenizer.from_pretrained("decapoda-research/llama-7b-hf")
-model = LlamaForCausalLM.from_pretrained("decapoda-research/llama-7b-hf").cuda()
+model = LlamaForCausalLM.from_pretrained("decapoda-research/llama-7b-hf", device_map='audio', load_in_8bit=True)
+
+for param in model.parameters():
+  param.requires_grad = False  # freeze the model - train adapters later
+  if param.ndim == 1:
+    # cast the small parameters (e.g. layernorm) to fp32 for stability
+    param.data = param.data.to(torch.float32)
+
+model.gradient_checkpointing_enable()  # reduce number of stored activations
+model.enable_input_require_grads()
+
+class CastOutputToFloat(nn.Sequential):
+  def forward(self, x): return super().forward(x).to(torch.float32)
+model.lm_head = CastOutputToFloat(model.lm_head)
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    target_modules=["q_proj", "v_proj"],
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM"
+)
+
+model = get_peft_model(model, config)
+print_trainable_parameters(model)
 
 dataset_f = open(dataset_fn, 'r', encoding='utf-8')
 dataset = BloomTranscript(dataset_f.read(), tokenizer)
 dataset_f.close()
 
-torch.cuda.empty_cache()
-
-training_args = TrainingArguments(output_dir=model_dir,
-                                  num_train_epochs=2,
-                                  logging_steps=10,
-                                  # save_steps=100,
-                                  save_steps=5000,
-                                  per_device_train_batch_size=1,
-                                  per_device_eval_batch_size=1,
-                                  warmup_steps=10,
-                                  weight_decay=0.05,
-                                  logging_dir=model_dir,
-                                  report_to='none',
-                                  fp16=True,
-                                  gradient_accumulation_steps=4,
-                                  gradient_checkpointing=True)
-
-trainer = Trainer(model=model,
-        args=training_args,
-        train_dataset=dataset,
-        data_collator=lambda data: {
-                'input_ids': torch.stack([f[0] for f in data]),
-                'attention_mask': torch.stack([f[1] for f in data]),
-                'labels': torch.stack([f[0] for f in data])
-            }
-        )
-
+trainer = Trainer(
+    model=model, 
+    train_dataset=dataset,
+    args=TrainingArguments(
+        per_device_train_batch_size=4, 
+        gradient_accumulation_steps=4,
+        warmup_steps=100, 
+        max_steps=200, 
+        learning_rate=2e-4, 
+        fp16=True,
+        logging_steps=1, 
+        output_dir=model_dir
+    ),
+    data_collator=DataCollatorForLanguageModeling(tokenizer, mlm=False)
+)
+model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
 trainer.train()
+
 trainer.save_model(model_dir + 'llama_final.pt')
 
